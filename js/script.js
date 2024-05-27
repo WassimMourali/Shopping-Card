7608774aabb2b692fa44bf684317c268974fe5ac
wassim mourali <wassim.mourali@rose-blanche.com>
2024-05-27 21:20:50 +0100

    avec des commantaire

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+//script qui permet de mettre l'écoute de fonction global en écoute
 document.addEventListener("DOMContentLoaded", function() {
+
+    //déclaration des éléments html en code script
     const plusButtons = document.querySelectorAll(".fas.fa-plus-circle");
     const minusButtons = document.querySelectorAll(".fas.fa-minus-circle");
     const deleteButtons = document.querySelectorAll(".fas.fa-trash-alt");
     const likeButtons = document.querySelectorAll(".fas.fa-heart");
 
+//fonction pour fait l'incementation la quantité au panier en clic sur bouton +
     plusButtons.forEach(button => {
         button.addEventListener("click", function() {
             const quantityElement = button.nextElementSibling;
@@ -14,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+//fonction pour fait l'decrementation la quantité au panier en clic sur bouton -
     minusButtons.forEach(button => {
         button.addEventListener("click", function() {
             const quantityElement = button.previousElementSibling;
@@ -26,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+//fonction qui permet de supprimer l'article au niveau d'interface client si en clic sur bouton supprimer
     deleteButtons.forEach(button => {
         button.addEventListener("click", function() {
             const cardBody = button.closest(".card-body");
@@ -34,12 +40,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+//fonction qui permet de activer ou deactiver icon j'aime par si en rouge (active) si non (déactivé)
     likeButtons.forEach(button => {
         button.addEventListener("click", function() {
             button.classList.toggle("text-danger");
         });
     });
 
+//fonction pour mettre la mise à jour de total prix et calculer le montant par méthode prix article * nombre d'unité
     function updateTotalPrice() {
         let totalPrice = 0;
         const unitPrices = document.querySelectorAll(".unit-price");
@@ -50,3 +58,4 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector(".total").textContent = totalPrice + " $";
     }
 });
+
